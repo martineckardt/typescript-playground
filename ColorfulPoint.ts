f5aed7bf40643b49197bbd3deaa73bfdb7bc1ef4
MartinEckardt <m.eckardt@outlook.com>
2022-06-06 13:30:02 +0200

    Capitalize Filename

--- ColorfulPoint.ts
+++ ColorfulPoint.ts
@@ -1,4 +1,4 @@
-import Point from "./point";
+import Point from "./Point";
 import IColorful, { Color } from "./IColorful";
 
 class ColorfulPoint extends Point implements IColorful {
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import Point from "./point";
+import Point from "./Point";
 import ColorfulPoint from "./ColorfulPoint";
 
 
